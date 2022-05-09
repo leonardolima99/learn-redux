e91574b20c801412c23b6579ea9ed09c6056c927
Leonardo Lima <dev.leonardolima@gmail.com>
2022-05-09 14:48:19 -0400

    add usage counter global state in app.tsx

--- redux3/src/App.jsx
+++ redux3/src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useDispatch, useSelector } from "react-redux";
+import { selectCount } from "./store/Counter/selectors";
+import { increment } from "./store/Counter/actions";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  const count = useSelector(selectCount);
 
   return (
     <div className="App">
@@ -11,7 +16,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => dispatch(increment())}>
             count is: {count}
           </button>
         </p>
@@ -27,7 +32,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +44,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
