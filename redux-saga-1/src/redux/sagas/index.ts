090078d1b85269d0eea023f02ab122effaf3da7e
Leonardo Lima <dev.leonardolima@gmail.com>
2022-05-12 07:47:38 -0400

    config redux saga with generator function and yield

--- redux-saga-1/src/redux/sagas/index.ts
+++ redux-saga-1/src/redux/sagas/index.ts
@@ -1,1 +1,6 @@
-export * from "./user";
+import { all } from "redux-saga/effects";
+import user from "./user";
+
+export default function* rootSaga(): Generator<any> {
+  return yield all([user]);
+}
--- redux-saga-1/src/redux/sagas/user/index.ts
+++ redux-saga-1/src/redux/sagas/user/index.ts
@@ -1,4 +1,26 @@
+import { call, put, all, takeLatest } from "redux-saga/effects";
 import { UserType } from "../../../types";
+import { getUserFailure, getUserSuccess } from "../../actions";
 import * as types from "../../types";
 
 let userApi: UserType;
+const userRequest = async (name: string) => {
+  try {
+    const request = await fetch(`https://api.github.com/users/${name}`);
+    userApi = await request.json();
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+export function* userData(action: { type: string; payload: string }) {
+  try {
+    yield call(userRequest, action.payload);
+    yield put(getUserSuccess(userApi));
+  } catch (e) {
+    yield put(getUserFailure({ message: "Falha ao requisitar." }));
+  }
+}
+
+export default all([takeLatest(types.GET_USER_REQUEST, userData)]);
